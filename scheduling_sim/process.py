eda0373a95381c3f52dbf03fe22369aa1b5cb952
Carlos Santos <cadu.vs@hotmail.com>
2023-08-25 01:18:41 -0300

    feat: total runtime property

--- scheduling_sim/process.py
+++ scheduling_sim/process.py
@@ -4,40 +4,41 @@ class Process:
     system resources and execution context.
 
     In this implementation of a Scheduling simulation, an object of type Process
-    has a duration (the time it takes to be executed), a priority level and an
-    arrival time, which represents the moment the Process was added to the Scheduler's
+    has an execution time (the time it takes to be executed), a priority level and
+    an arrival time, which represents the moment the Process was added to the Scheduler's
     queue.
     """
 
     def __init__(
-        self, duration: int = 0, priority_level: int = 1, arrival_time: int = 0
+        self, execution_time: int = 0, priority_level: int = 1, arrival_time: int = 0
     ):
-        self.duration = duration
+        self.execution_time = execution_time
+        self.total_runtime = execution_time
         self.priority_level = priority_level
         self.arrival_time = arrival_time
 
     def __repr__(self) -> str:
         return (
             "Process("
-            f"duration={self.duration}, "
+            f"execution_time={self.execution_time}, "
             f"priority_level={self.priority_level}, "
             f"arrival_time={self.arrival_time}"
             ")"
         )
 
     @property
-    def duration(self) -> int:
-        """The execution time of the process."""
-        return self._duration
+    def execution_time(self) -> int:
+        """The execution time (duration) of the process."""
+        return self._execution_time
 
-    @duration.setter
-    def duration(self, value: int):
+    @execution_time.setter
+    def execution_time(self, value: int):
         if type(value) != int:
             raise TypeError(
-                f"process duration should be an integer. Got {type(value)} instead."
+                f"process execution time should be an integer. Got {type(value)} instead."
             )
 
-        self._duration = value
+        self._execution_time = value
 
     @property
     def priority_level(self) -> int:
@@ -74,3 +75,23 @@ class Process:
             raise ValueError(f"arrival time should be positive. Got {value} instead.")
 
         self._arrival_time = value
+
+    @property
+    def total_runtime(self) -> int:
+        """The total runtime the process needed to finish (execution time + waiting)."""
+        return self._total_runtime
+
+    @total_runtime.setter
+    def total_runtime(self, value: int):
+        if type(value) != int:
+            raise TypeError(
+                f"total runtime should be an integer. Got {type(value)} instead."
+            )
+
+        if value < 0:
+            raise ValueError(f"total runtime should be positive. Got {value} instead.")
+
+        if value < self.execution_time:
+            raise ValueError("total runtime should not be lower than execution time.")
+
+        self._total_runtime = value
