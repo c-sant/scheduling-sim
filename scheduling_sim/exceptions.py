4dfddd109139f73adc5bd134cf96bc7d9ec6befa
Carlos Santos <cadu.vs@hotmail.com>
2023-09-09 11:00:03 -0300

    feat: InvalidProcessQueueError exception

--- scheduling_sim/exceptions.py
+++ scheduling_sim/exceptions.py
@@ -35,8 +35,9 @@ class InvalidProcessNameError(ValueError):
 class NoProcessWithArrivalTimeZeroError(Exception):
     """Exception raised when none of the processes have an arrival time of 0.
 
-    This exception is raised when a scheduling algorithm or process management code expects at least one
-    process to have an arrival time of 0 but none of the processes meet this criteria.
+    This exception is raised when a scheduling algorithm or process management code
+    expects at least one process to have an arrival time of 0 but none of the processes
+    meet this criteria.
 
     Attributes:
         message (str): A custom error message describing the exception.
@@ -48,16 +49,33 @@ class NoProcessWithArrivalTimeZeroError(Exception):
 
 
 class NoProcessesInQueueError(Exception):
-    """
-    Exception raised when there are no processes in the queue.
+    """Exception raised when there are no processes in the queue.
 
-    This exception is raised when a scheduling algorithm or process management code expects to operate
-    on a queue of processes but encounters an empty queue.
+    This exception is raised when a scheduling algorithm or process management code
+    expects to operate on a queue of processes but encounters an empty queue.
 
     Attributes:
         message (str): A custom error message describing the exception.
     """
 
     def __init__(self, message="The queue does not contain any processes."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class InvalidProcessQueueError(TypeError):
+    """Exception raised when the processes queue contains objects that are not of type Process.
+
+    This exception is raised when a Scheduler attempts to operate on a processes
+    queue that contains objects of an incorrect type.
+
+    Attributes:
+        message (str): A custom error message describing the exception.
+    """
+
+    def __init__(
+        self,
+        message="Invalid process queue. The queue must be a list and all elements must be of type Process.",
+    ):
         self.message = message
         super().__init__(self.message)
